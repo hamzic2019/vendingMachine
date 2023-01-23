9aada6bdae97a3aa74892c20fb887c0656639779
hamzic2019 <hamzicharis96@gmail.com>
2023-01-23 20:30:00 +0100

    fixing typo

--- server/app.js
+++ server/app.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './teampltes/views'));
-hbs.registerPartials(path.join(__dirname, './teampltes/partials'));
+app.set('views', path.join(__dirname, './templates/views'));
+hbs.registerPartials(path.join(__dirname, './templates/partials'));
 
 app.listen(PORT, () => {
     console.log(`Application is up and running!`);
