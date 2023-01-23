5d12449d9d928dde354baf19a7aa2cf77b50434f
hamzic2019 <hamzicharis96@gmail.com>
2023-01-23 22:46:49 +0100

    add authentication middleware using jsonwebtoken and mongoose

---
+++ server/middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../database/models/User');
+const dotenv = require('dotenv').config();
+
+// Middleware function to handle authentication
+const auth = async (req, res, next) => {
+    // Get the token from the headers
+    let token = req.headers["authorization"] //.replace('Bearer ', '');
+    
+    // Check if the token is undefined
+    if(token === undefined){
+        req.logged = false;
+        next();
+    }else {
+        // Remove the 'Bearer ' prefix from the token
+        token = token.replace('Bearer ', '');
+
+        // Verify the token
+        const isValid = jwt.verify(token, `${process.env.TOKEN_SECRET}`)
+        
+        // Find the user with the same id as the token payload
+        const user = await User.findOne({_id: isValid._id});
+
+         // Set the user, token, and logged value on the request object
+        req.user = user;
+        req.token = token; 
+        req.logged = true;
+
+        // Call the next middleware function
+        next();
+    }
+}
+
+module.exports = auth;
